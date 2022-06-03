4bb3adbfa9cee219ad953c5ce52d04c2cd2400cf
Logan Renaud <loganrenaud@hotmail.fr>
2022-06-03 16:32:53 +0200

    test

--- test_1.py
+++ test_1.py
@@ -1,26 +1,24 @@
+import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import string
+import unidecode
 
-text='hello, this is a text, bonjour ceci est un texte'
 
 
-def article_tokenize(text):
-    if type(text)!= str:
+def article_tokenize(article):
+    article=article.replace("'"," ")
+    article=unidecode.unidecode(article)
+    if type(article)!= str:
         raise Exception("The function takes a string as input data")
     else:
-        # 1. On extrait les abbreviations
-        tokenizer = RegexpTokenizer('[a-zA-Z]\.[a-zA-Z]')#Construction de l'objet tokenizer
-        abrevtokens = tokenizer.tokenize(text)#application de la methode tokenize() a l'objet lui permettant d'extraire les abreviations
-        # 2. On extrait les mots et les nombres
-        tokenizer = RegexpTokenizer('[a-zA-Z]{2,}|\d+\S?\d*')
-        wordtokens = tokenizer.tokenize(text)
-        return abrevtokens + wordtokens
+        tokens=word_tokenize(article)
+        return tokens
 
-Stopwords=set(stopwords.words("english"))
-Stopwords=Stopwords.union(set(stopwords.words("french")))
-
-#Ajouter méthode td 1 ?
-tokenized_corpus=article_tokenize(text)
 
 def remove_stop_words(texte_token ,stop_word):
     res=[]
@@ -30,6 +28,36 @@ def remove_stop_words(texte_token ,stop_word):
     return res
     
 
-filtered_collection = remove_stop_words(tokenized_corpus,Stopwords)
-print(filtered_collection)
+def collection_stemming(segmented_collection):
+    stemmed_collection=[]
+    stemmer = SnowballStemmer(language='french')
+    #stemmer = PorterStemmer ()
+    for i in segmented_collection:
+        stemmed_collection.append(stemmer.stem(i))
+    return stemmed_collection
+
+
+def collection_lemmatize(segmented_collection):
+    res=[]
+    stemmer = WordNetLemmatizer() # initialisation d'un lemmatiseur
+    for i in segmented_collection:
+        res.append( stemmer.lemmatize(i))
+    return res
+
+
+
+Stopwords=set(stopwords.words("english"))
+Stopwords=Stopwords.union(set(stopwords.words("french")))
+Stopwords=Stopwords.union(set(string.punctuation))
+
+
+def segmentation (text):
+    tokenized_corpus=article_tokenize(text)
+    filtered_collection = remove_stop_words(tokenized_corpus,Stopwords)
+    lemmatized_collection=collection_lemmatize(filtered_collection)
+    stemmed_collection=collection_stemming(lemmatized_collection)
+    final=remove_stop_words(stemmed_collection,Stopwords)
+    return(final)
+
+
 
