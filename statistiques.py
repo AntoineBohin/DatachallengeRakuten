ca21121c4763f08d9e41a560633c84821e37945b
Clément Ribe <clement.ribe@student-cs.fr>
2022-06-07 08:46:08 +0200

    changements fichier statistiques

--- statistiques.py
+++ statistiques.py
@@ -2,15 +2,12 @@ import numpy as np
 import math as m 
 import pandas as pd 
 import matplotlib.pyplot as plt
-import spacy
-
-from spacy_langdetect import LanguageDetector
+from textblob import TextBlob
+from charset_normalizer import detect
 from sympy import rotations
 
 df_x = pd.read_csv('./dataset/X_train_update.csv')
 df_y = pd.read_csv('./dataset/Y_train_CVw08PX.csv')
-
-print(m.isnan(df_x.iloc[5]['description']))
 
 ## on compte le nombre d'occurences de chaque classe
 classes = {}
@@ -53,26 +50,27 @@ def pagesVides():
     pagesVides['non_vide'] = 0
     for i in range(len(df_x)):
         description = df_x.iloc[i]['description']
-        if m.isnan(description) :
-            print(description)
+        if type(description) == float and m.isnan(description) :
             pagesVides['vide'] +=1
         else: 
             pagesVides['non_vide'] += 1
+    n = somme(pagesVides)
+    pagesVides['vide'] /= n
+    pagesVides['non_vide'] /= n 
     return pagesVides
-
-print(pagesVides())
 
 ## on compte le nombre de description en langue autre que le français 
 def langues():
-    nlp = spacy.load('xx_ent_wiki_sm')
-    nlp.add_pipe(LanguageDetector(), name='language_detector', last=True)
     langues = {}
     for i in range(len(df_x)):
         description = df_x.iloc[i]['description']
-        doc = nlp(description)
-        detect_language = doc._.language
-        if not detect_language in langues.keys() :
-            langues[detect_language] = 1
-        else: 
-            langues[detect_language] += 1
+        if not type(description) == float: 
+            doc = TextBlob(description)
+            detect_language = doc.detect_language()
+            if not detect_language in langues.keys() :
+                langues[detect_language] = 1
+            else: 
+                langues[detect_language] += 1
     return langues
+
+print(langues())
