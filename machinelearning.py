be4c7353df2cb187219ae4372faf01abd48dfb68
Antoine Bohin <antoine.bohin@student-cs.fr>
2022-06-07 11:46:08 +0200

    Premier modèle de ML

---
+++ machinelearning.py
@@ -0,0 +1,24 @@
+from lightgbm import train
+from sklearn.naive_bayes import MultinomialNB
+from scipy import sparse
+import pandas as pd
+
+train_csv = pd.read_csv('processed_and_concatenated_X_Y_train.csv', index_col=0)
+
+X_tfidf_sample = sparse.load_npz("./X_consolidated_without_numbers.npz")
+
+
+
+clf= MultinomialNB().fit(X_tfidf_sample[0:80000],train_csv["ProductTypeCode"][0:80000])
+
+
+good_predictions=0
+number_of_predictions=0
+for k in range(80001,84915):
+    predicted=clf.predict(X_tfidf_sample[k])
+    number_of_predictions+=1
+    if predicted[0]==train_csv["ProductTypeCode"][k]:
+        good_predictions+=1
+    print(k,predicted,train_csv["ProductTypeCode"][k])
+
+print("Global Accuracy is",good_predictions/number_of_predictions)
--- text_treatment.py
+++ text_treatment.py
@@ -4,7 +4,14 @@ import nltk
 import re
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 import pandas as pd
+import unidecode
+import string
+
 
 #from transformers import CamembertTokenizer 
 """
@@ -20,6 +27,9 @@ def cleanhtml(raw_html):
     cleansp = re.sub('\s+', ' ', cleantext)
     return cleansp
 
+def to_string(classe):
+    return(str(classe))
+
 def description_and_title_clean(dataframe):
     dataframe["Description"].apply(cleanhtml)
     dataframe["Title"].apply(cleanhtml)
@@ -30,36 +40,24 @@ def process_csv(csv_path_X,csv_path_Y):
     train= X_train_df.merge(Y_train_df[['IntegerID', 'ProductTypeCode']], how = 'left', on = 'IntegerID')
     train["Title"]=train["Title"].astype("string")
     train["Description"]=train["Description"].astype("string")  
-    
-    train["Description"]=train["Description"].apply(cleanhtml)
-    train["Title"]=train["Title"].apply(cleanhtml)
     return(train)
 
-train=process_csv('./dataset/X_train_update.csv','./dataset/Y_train_CVw08PX.csv')
-print(train)
-
-k=30
-print(train["Description"][k])
-text=train["Description"][k]
-
-
-def article_tokenize(text):
-    if type(text)!= str:
+def article_tokenize(article):
+    article=article.replace("'"," ")
+    for i in range(10):
+        article=article.replace(str(i)," ")
+    article=unidecode.unidecode(article)
+    if type(article)!= str:
         raise Exception("The function takes a string as input data")
     else:
-        # 1. On extrait les abbreviations
-        tokenizer = RegexpTokenizer('[a-zA-Z]\.[a-zA-Z]')#Construction de l'objet tokenizer
-        abrevtokens = tokenizer.tokenize(text)#application de la methode tokenize() a l'objet lui permettant d'extraire les abreviations
-        # 2. On extrait les mots et les nombres
-        tokenizer = RegexpTokenizer('[a-zA-Z]{2,}|\d+\S?\d*')
-        wordtokens = tokenizer.tokenize(text)
-        return abrevtokens + wordtokens
+        tokens=word_tokenize(article)
+        return tokens
 
 Stopwords=set(stopwords.words("english"))
 Stopwords=Stopwords.union(set(stopwords.words("french")))
+Stopwords=Stopwords.union(set(string.punctuation))
 
 #Ajouter méthode td 1 ?
-tokenized_corpus=article_tokenize(text)
 
 def remove_stop_words(texte_token ,stop_word):
     res=[]
@@ -67,7 +65,77 @@ def remove_stop_words(texte_token ,stop_word):
         if i not in stop_word:
             res.append(i)
     return res
-    
 
-filtered_collection = remove_stop_words(tokenized_corpus,Stopwords)
-print(filtered_collection)
+def collection_stemming(segmented_collection):
+    stemmed_collection=[]
+    stemmer = SnowballStemmer(language='french')
+    #stemmer = PorterStemmer ()
+    for i in segmented_collection:
+        stemmed_collection.append(stemmer.stem(i))
+    return stemmed_collection
+
+def collection_lemmatize(segmented_collection):
+    res=[]
+    stemmer = WordNetLemmatizer() # initialisation d'un lemmatiseur
+    for i in segmented_collection:
+        res.append( stemmer.lemmatize(i))
+    return res
+
+def segmentation(text):
+    tokenized_corpus=article_tokenize(text)
+    filtered_collection = remove_stop_words(tokenized_corpus,Stopwords)
+    lemmatized_collection=collection_lemmatize(filtered_collection)
+    stemmed_collection=collection_stemming(lemmatized_collection)
+    final=remove_stop_words(stemmed_collection,Stopwords)
+    return(final)
+
+
+def concatenate_text(list):
+    string=""
+    for k in range(len(list)):
+        string=string+list[k]+" "
+    return(string)
+
+
+def text_treatment_of_the_csv(dataframe):
+    for k in range(len(dataframe)):
+        description=dataframe["Description"][k]
+        title=dataframe["Title"][k]
+        try:    
+            dataframe["Title"][k]=cleanhtml(dataframe["Title"][k])
+        except:
+            """do nothing"""
+        try:
+            dataframe["Description"][k]=cleanhtml(dataframe["Description"][k])
+        except:
+            """do nothing"""
+        description=segmentation(str(description))
+        description=concatenate_text(description)
+        title=segmentation(str(title))
+        title=concatenate_text(title)
+        dataframe["Description"][k]=description
+        dataframe["Title"][k]=title
+    return(dataframe)
+
+def csv_pipeline(csv_path_X,csv_path_Y):
+    train=process_csv(csv_path_X,csv_path_Y)
+    train=text_treatment_of_the_csv(train)
+    print(train)
+    return(train)
+
+
+train=csv_pipeline('./dataset/X_train_update.csv','./dataset/Y_train_CVw08PX.csv')
+
+#filtered_collection = remove_stop_words(tokenized_corpus,Stopwords)
+#print(filtered_collection)
+
+train.to_csv('processed_X_Y_train.csv', index=False)
+
+
+tfidf = TfidfVectorizer()
+
+"""
+X_tfidf_sample= tfidf.fit_transform(seq)
+print("Shape of the TF-IDF Matrix:")
+print(X_tfidf_sample)
+"""
